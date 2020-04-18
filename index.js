354974d39f26a9847ed8cf7d28d42e2c3a66a470
SkeeperBooss <63904757+SkeeperBooss@users.noreply.github.com>
2020-04-18 12:25:07 +0200

    init
    init

---
+++ index.js
@@ -0,0 +1,45 @@
+const discord = require("discord.js");
+const botConfig = require("./botconfig.json");
+
+const bot = new discord.Client();
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is online!`)
+
+    bot.user.setActivity("Testing", { type: "PLAYING" });
+
+});
+
+
+bot.on("message", async message => {
+    // Als de bot een bericht stuurt stuur dan return
+    if (message.author.bot) return;
+
+
+    if (message.channel.type === "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+    
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    if( command === `${prefix}hallo`){
+
+        return message.channel.send("Hallo");
+
+    }
+    if( command === `${prefix}hey`){
+
+        return message.channel.send("hey");
+
+    }
+
+});
+
+
+bot.login(process.env.token);
